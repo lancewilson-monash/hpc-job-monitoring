72c47671542b0a99ae325a02d7361791c9072f67
lancewilson-monash <lance.wilson@monash.edu>
2019-02-27 20:16:10 +1100

    Add in job type to file time processing script

--- job-runtime-files.bash
+++ job-runtime-files.bash
@@ -11,6 +11,7 @@ LAST=$(date "+%s" -d "$( ls --full-time -rt $1 |tail -n1 |awk '{print $6,$7,$8}'
 #echo $LAST
 JOBTIME=$(expr $LAST - $FIRST)
 JOBNAME=$( echo $1 | rev | cut -d"/" -f1 |rev) 
-echo "$JOBNAME,$JOBTIME"
+JOBTYPE=$( echo $1 | rev | cut -d"/" -f2 |rev) 
+echo "$JOBNAME,$JOBTYPE,$JOBTIME"
 #echo "$JOBNAME Total Job Time = $JOBTIME"
 #echo $JOBTIME | awk '{printf("%d:%02d:%02d:%02d\n",($1/60/60/24),($1/60/60%24),($1/60%60),($1%60))}' 
