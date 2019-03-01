b19c1070bbd1843ed3b3cebbdd9ab737818a8818
lancewilson-monash <lance.wilson@monash.edu>
2019-03-01 20:47:30 +1100

    Fix sed to only replace run directory not input

--- class3d-extract.bash
+++ class3d-extract.bash
@@ -8,10 +8,12 @@ cp blank-template.bash $JOB_FOLDER-template.bash
 # Extract command and insert into template
 JOB_COMMAND=$(grep --color=never -i relion $(find ./Class3D -type d -iname "job*" |head -n1)/note.txt)
 sed -i "s~REPLACECOMMAND~mpirun $JOB_COMMAND~g" $JOB_FOLDER-template.bash
-# Check job template scrip/runt creates folders in the correct location
+# Check job template script/run creates folders in the correct location
 sed -i "s~REPLACEFOLDER~Class3D/$JOB_FOLDER~g" $JOB_FOLDER-template.bash
-sed -i 's~/run~-$LOGDATE/run~g' $JOB_FOLDER-template.bash
+sed -i 's~/run ~-$LOGDATE/run~g' $JOB_FOLDER-template.bash
 # Remove gpu assignation if they exist
 sed -i 's~--gpu "[0-9:]*"~--gpu ""~g' $JOB_FOLDER-template.bash
 # Remove any thread settings
 sed -i 's~--j [0-9].~--j 1~g' $JOB_FOLDER-template.bash
+
+
