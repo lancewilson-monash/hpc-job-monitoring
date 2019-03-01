5977aeebcb182b69afc6b4d62d03e921c1e882dc
lancewilson-monash <lance.wilson@monash.edu>
2019-03-02 09:23:00 +1100

    Fix problem with sed command that removed a necessary space in the input definition

--- class3d-extract.bash
+++ class3d-extract.bash
@@ -10,7 +10,7 @@ JOB_COMMAND=$(grep --color=never -i relion $(find ./Class3D -type d -iname "job*
 sed -i "s~REPLACECOMMAND~mpirun $JOB_COMMAND~g" $JOB_FOLDER-template.bash
 # Check job template script/run creates folders in the correct location
 sed -i "s~REPLACEFOLDER~Class3D/$JOB_FOLDER~g" $JOB_FOLDER-template.bash
-sed -i 's~/run ~-$LOGDATE/run~g' $JOB_FOLDER-template.bash
+sed -i 's~/run ~-$LOGDATE/run ~g' $JOB_FOLDER-template.bash
 # Remove gpu assignation if they exist
 sed -i 's~--gpu "[0-9:]*"~--gpu ""~g' $JOB_FOLDER-template.bash
 # Remove any thread settings
