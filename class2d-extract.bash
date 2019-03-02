e0beb1f8fe3792bd59e1646778483558831846ca
lancewilson-monash <lance.wilson@monash.edu>
2019-03-02 12:07:18 +1100

    Apply fixes from class3d to class2d scripts

--- class2d-extract.bash
+++ class2d-extract.bash
@@ -7,10 +7,10 @@ JOB_FOLDER=$(find ./Class2D -type d -iname "job01*" |head -n1 |rev| cut --charac
 cp blank-template.bash $JOB_FOLDER-template.bash
 # Extract command and insert into template
 JOB_COMMAND=$(grep --color=never -i relion $(find ./Class2D -type d -iname "job01*" |head -n1)/note.txt)
-sed -i "s~REPLACECOMMAND~mpirun $JOB_COMMAND~g" $JOB_FOLDER-template.bash
-# Check job template scrip/runt creates folders in the correct location
+sed -i "s~REPLACECOMMAND~mpirun $JOB_COMMAND \&~g" $JOB_FOLDER-template.bash
+# Check job template script/run creates folders in the correct location
 sed -i "s~REPLACEFOLDER~Class2D/$JOB_FOLDER~g" $JOB_FOLDER-template.bash
-sed -i 's~/run~-$LOGDATE/run~g' $JOB_FOLDER-template.bash
+sed -i 's~/run ~-$LOGDATE/run ~g' $JOB_FOLDER-template.bash
 # Remove gpu assignation if they exist
 sed -i 's~--gpu "[0-9:]*"~--gpu ""~g' $JOB_FOLDER-template.bash
 # Remove any thread settings
