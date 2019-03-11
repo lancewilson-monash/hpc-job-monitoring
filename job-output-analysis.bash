af65c574ab1e5134d68dd4bc6acdc516cccf36cc
lancewilson-monash <lance.wilson@monash.edu>
2019-03-11 11:39:46 +1100

    Fix cd to allow for spaces in path

--- job-output-analysis.bash
+++ job-output-analysis.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Script to take output files and extract information
 # Parameter 1 = Slurm JOBID
+INITIALDIR=`pwd`
 JOBID=$1
 ANALYSISFOLDER=loganalysis/$JOBID
 # Make a single directory for all files for a job
@@ -15,3 +16,10 @@ mv $JOBID.* $ANALYSISFOLDER/
 grep -l "JOBID=$JOBID" *.log
 FILEDATES=`grep -l "JOBID=$JOBID" *.log | cut -d'-' -f2 | cut -d'.' -f1`
 for DATE in $FILEDATES; do mv -v *-$DATE.log $ANALYSISFOLDER/ ;done
+
+# Filter log files to remove unwanted data
+cd $ANALYSISFOLDER
+for DATE in $FILEDATES; do cat nividia-$DATE.log | grep -v 'gpu\|Idx' > nividia-$DATE-filtered.log; done
+
+# Change back to original folder
+cd "$INITIALDIR"
